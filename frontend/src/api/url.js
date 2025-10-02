6f519ce01aae46f6ceb237a3203ea64193898a6a
Mohd Amaan <amaann167@gmail.com>
2025-10-03 00:37:02 +0530

    Update url.js

--- frontend/src/api/url.js
+++ frontend/src/api/url.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const BACKEND_URL = axios.create({
-    baseURL: "http://localhost:4001/api/v1/note-app/"
+    baseURL: "https://notes-app-backend-nvjl.onrender.com/api/v1/note-app/"
 })
 
 export default BACKEND_URL;
